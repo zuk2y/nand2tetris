34becb5f9c9f0dd89e562d56f93936967f1e570a
zuk2y <suz.liang.oji@gmail.com>
2020-05-02 15:29:52 +0900

    [04] Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M=1
+@R2
+M=0
+(LOOP)
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JGT
+@R0
+D=M
+@R2
+M=M+D
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
