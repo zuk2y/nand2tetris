f384fcac1dd54fd9ce993407e6883eebbbcf1cf5
zuk2y <suz.liang.oji@gmail.com>
2021-03-21 16:10:41 +0900

    create SymbolTable.java

--- projects/06/HackAssembler/src/SymbolTable.java
+++ projects/06/HackAssembler/src/SymbolTable.java
@@ -1,3 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SymbolTable {
-    
+
+    private Map<String, Integer> symbolTable;
+
+    public SymbolTable(){
+        symbolTable = new HashMap<String, Integer>();
+    }
+
+    public void addEntry(String symbol, int address){
+        symbolTable.put(symbol, address);
+    }
+
+    public boolean contains(String symbol){
+        return symbolTable.containsKey(symbol);
+    }
+
+    public int getAddress(String symbol){
+        return symbolTable.get(symbol);
+    }
 }
